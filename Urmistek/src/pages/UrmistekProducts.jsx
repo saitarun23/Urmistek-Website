4d9684902fb7861e2cf3fb49d4786b23c431393a
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2026-06-01 22:35:52 +0530

    done

--- Urmistek/src/pages/UrmistekProducts.jsx
+++ Urmistek/src/pages/UrmistekProducts.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import '../styles/UrmistekProducts.css';
+import '../styles/urmistekProducts.css';
 
 // Custom Minimalist SVG Icons
 const SearchIcon = () => (
