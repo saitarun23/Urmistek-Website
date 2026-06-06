2e5d5320a47a078d6ed4e7812e7d6d1d7740562c
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2026-06-06 10:27:10 +0530

    done

--- Urmistek/src/components/SplashScreen.jsx
+++ Urmistek/src/components/SplashScreen.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../styles/SplashScreen.css";
-import logo1 from "../assets/images/urm.png";
+import logo1 from "../assets/images/Urm.png";
 
 const SplashScreen = ({ onFinish }) => {
   const [visible, setVisible] = useState(true);
